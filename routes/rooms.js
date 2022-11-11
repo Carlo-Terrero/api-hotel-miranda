da66cc505d271c2f58132dd3548cf0d6ed5f0e61
Carlos Terrero <carlosjose2111@gmail.com>
2022-11-11 11:08:20 +0100

    add omit=dev

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,22 +1,18 @@
+'use strict'
+
 const express = require('express');
 const router = express.Router();
 
-// Importamos los controladores
 const roomController = require('../controllers/roomController')
 
-// Cogemos todas las rooms
 router.get('/', roomController.room_get)
 
-// Seleccionamos la room de la id
 router.get('/:id', roomController.room_getOne)
 
-// Actualizamos la room de la id
 router.put('/:id', roomController.room_put)
 
-// Eliminamos la room del id
 router.delete('/:id', roomController.room_delete)
 
-// Añadimos una room
 router.post('/', roomController.room_post)
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,18 @@
+'use strict'
+
 const express = require('express');
 const router = express.Router();
 
-//importamos controladores
 const userController = require('../controllers/userController')
 
-//La parte que esta comentada es sin controladores, el resto si
-
-/* GET users listing. */
 router.get('/', userController.user_get);
 
-// Cogemos un user
 router.get('/:id', userController.user_getOne);
 
-// Añadimos un user
 router.post('/', userController.user_post);
 
-// Actualizamos  un user
 router.put('/:id', userController.user_put);
 
-// Eliminamos un user
 router.delete('/:id', userController.user_delete);
 
 module.exports = router;
